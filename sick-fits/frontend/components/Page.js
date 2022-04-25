b49b4bb70e93b501ceacc9b482a30b73a2102d48
erinpikachu <e.foster945@gmail.com>
2022-04-25 12:55:30 -0400

    creating page layout component

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,16 @@
+import { PropTypes } from 'prop-types';
+
+Page.propTypes = {
+  cool: PropTypes.string,
+  children: PropTypes.any,
+};
+
+export default function Page({ children, cool }) {
+  return (
+    <div>
+      <h2>I am the page component</h2>
+      <h3>{cool}</h3>
+      {children}
+    </div>
+  );
+}
